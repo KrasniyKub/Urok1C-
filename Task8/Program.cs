51e86ad27f0c451ea9daa28f935bf875b1002b89
Dmitriy Sereda <nadechke@gmail.com>
2023-09-12 20:34:57 +0300

    Задание 8

---
+++ Task8/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа которая принимает число N и показывает все четные числа от числа 1 до числа N
+Console.Write("Введите число/ ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+while(count < num)
+{
+    count++;
+    if(count%2 == 0)
+    {
+        Console.Write(count);
+        Console.Write(", ");
+    }
+}
